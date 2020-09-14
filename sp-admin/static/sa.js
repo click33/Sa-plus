e86387b42a7c350b707dbbb5c05e1b0c1a8ebc13
click33 <36243476+click33@users.noreply.github.com>
2020-09-14 23:09:46 +0800

    修复表格动画bug
    修复表格动画bug

--- sp-admin/static/sa.js
+++ sp-admin/static/sa.js
@@ -992,7 +992,7 @@ var sa = {
 		// 刷新表格高度, 请务必在所有表格高度发生变化的地方调用此方法
 		me.f5TableHeight = function() {
 			Vue.nextTick(function() {
-				if($('.el-table .el-table__body-wrapper table').length == 0) {
+				if($('.el-table.data-table .el-table__body-wrapper table').length == 0) {
 					return;
 				}
 				setTimeout(function() {
